259ac510990d0020d77abcbab231032e60ce2b37
RynerNO <ryner.no@gmail.com>
2020-08-17 08:49:14 +0300

    rewrite everything within one class, added loggers

--- src/index.ts
+++ src/index.ts
@@ -1,155 +1,142 @@
 import debug from 'debug';
-const log = debug('discord-cmd-parser');
-
+const log = debug('Parser:INFO');
+const logDeep = debug('Parser:DEEP_INFO');
 interface options {
 	prefix?: string;
 	includeCmd: boolean;
 	useQuotes?: boolean;
 	quotesType?: string;
 	namedSeparator?: string;
-	commonSeparator?: string;
 }
-interface argsObj {
-	[key: string]: any;
-}
-interface argsDef {
-	name: string;
-	position?: number;
+interface args {
+	_?: Array<string>;
+	[name: string]: any;
 }
 
-let pOptions: options = {
+interface dirtArgs {
+	named: args;
+	unNamed: Array<string>;
+}
+
+const deffaultOptions: options = {
 	prefix: '',
 	includeCmd: false,
 	useQuotes: true,
 	quotesType: '"',
 	namedSeparator: '-',
-	commonSeparator: ' ',
 };
-
-function parseArg(
-	charArr: Array<string>,
-	indexFrom: number,
-	options: options,
-	result: Array<string> = []
-): argsObj {
-	if (
-		charArr[indexFrom] === options.namedSeparator ||
-		charArr[indexFrom] === options.commonSeparator ||
-		indexFrom >= charArr.length
-	) {
-		return {
-			index: indexFrom,
-			arg: result.join(''),
-		};
-	}
-	result.push(charArr[indexFrom]);
-	return parseArg(charArr, indexFrom + 1, options, result);
+export interface Parser {
+	pOptions: options;
 }
-
-function customSplit(string: string): Array<string> {
-	const splitedString: Array<string> = [];
-	string = string + pOptions.commonSeparator;
-	for (let i = 0; i < string.length; i++) {
-		if (string[i] === pOptions.commonSeparator) {
-			if (pOptions.useQuotes && string[0] === pOptions.quotesType) {
+export class Parser {
+	constructor(options?: options) {
+		this.pOptions = { ...deffaultOptions, ...options };
+	}
+	// Split string into args by defined separators
+	private _split(dirtString: string): Array<string> {
+		// Replace multiple spaces or new lines for correct spliting
+		log(`Spliting string: |${dirtString}|`);
+		let string = dirtString.replace(/\s\s+/g, ' ');
+		log(`Removed whitespaces and new lines: |${string}|`);
+		const splitedString: Array<string> = [];
+		for (let i = 0; i < string.length; i++) {
+			logDeep(`-------SPLITTING------- \nITERATION: ${i}\nSTRING: |${string}|`);
+			// Check for quotes
+			if (this.pOptions.useQuotes && string.startsWith(this.pOptions.quotesType)) {
+				logDeep(`------Find Quote------\nITERATION: ${i}\nSTRING: |${string}|`);
 				let nextQuote;
 
 				for (let j = 1; j < string.length; j++) {
-					if (string[j] === pOptions.quotesType) {
+					if (string[j] === this.pOptions.quotesType) {
+						logDeep(
+							`Second Quote\nITERATION: ${j}\nSTRING: |${string}|\n-----------------`
+						);
 						nextQuote = j;
 						break;
 					}
 				}
 
 				if (!nextQuote) {
+					logDeep(
+						`Second Quote NOT FOUND\nITERATION: ${i}\nSTRING: |${string}|\n-----------------`
+					);
 					splitedString.push(string.slice(0));
 					break;
 				}
-
 				splitedString.push(string.slice(1, nextQuote));
 				string = string.slice(nextQuote + 2);
 				i = 0;
 				continue;
 			}
-
-			splitedString.push(string.slice(0, i));
-
-			string = string.slice(i + 1);
-
-			i = 0;
+			// Check if char is whitespace
+			if (string[i].match(/\s/)) {
+				logDeep(
+					`Found whitespace\nITERATION: ${i}\nSTRING: |${string}|\n-----------------`
+				);
+				splitedString.push(string.slice(0, i));
+				string = string.slice(i + 1);
+				i = 0;
+			} else if (i === string.length - 1) {
+				logDeep(`STRING END\nITERATION: ${i}\nSTRING: |${string}|\n-----------------`);
+				splitedString.push(string.slice(0));
+			}
 		}
+		log(`String is splited: ${JSON.stringify(splitedString)}`);
+		return splitedString;
 	}
-	log(splitedString);
-	return splitedString;
-}
 
-function getNamedName(charArr: Array<string>, name?: string) {
-	if (charArr.length <= 0) return name;
-}
-function parseNamed(charArr: Array<string>) {
-	const name: Array<string> = [],
-		value: Array<string> = [];
-
-	charArr.splice(0, pOptions.namedSeparator.length);
-	while (charArr[0] !== pOptions.commonSeparator && charArr.length > 0) {
-		name.push(charArr.shift());
+	private _clearArgs(dirtArgs: dirtArgs, argsDef?: Array<string>): args {
+		let clearArgs: args = { ...dirtArgs.named };
+		log(`Clearing args\nArgs definition: ${argsDef}`);
+		if (!argsDef) return { ...dirtArgs.named, _: dirtArgs.unNamed };
+		argsDef.forEach((arg) => {
+			clearArgs[arg] = dirtArgs.unNamed.shift();
+		});
+		clearArgs._ = dirtArgs.unNamed;
+		log(`Args cleared: ${JSON.stringify(clearArgs)}`);
+		return clearArgs;
 	}
-	charArr.forEach((char, index) => {
-		name.push(charArr.shift());
-	});
-	charArr.shift();
-	while (charArr[0] !== pOptions.commonSeparator && charArr.length > 0) {
-		value.push(charArr.shift());
+
+	public async parse(string: string, argsDef?: Array<string>): Promise<args> {
+		log(`Got string: |${string}| \nGot args definition: [${argsDef}]`);
+		const splitedString = this._split(string);
+		const dirtArgs: dirtArgs = {
+			named: {},
+			unNamed: [],
+		};
+		log('Parsing splited');
+		while (splitedString.length !== 0) {
+			logDeep(
+				`-----PARSING SPLITED STRING-----\nSTRING: ${JSON.stringify(
+					splitedString
+				)}\nCURRENT ARG: ${splitedString[0]}`
+			);
+			if (splitedString[0].startsWith(this.pOptions.namedSeparator)) {
+				logDeep(
+					`-----FIND NAMED-----\nSTRING: ${JSON.stringify(splitedString)}\nCURRENT ARG: ${
+						splitedString[0]
+					}`
+				);
+				dirtArgs.named = {
+					...dirtArgs.named,
+					...{
+						[splitedString
+							.shift()
+							.split('')
+							.slice(this.pOptions.namedSeparator.length)
+							.join('')]: splitedString.shift(),
+					},
+				};
+
+				continue;
+			}
+			dirtArgs.unNamed.push(splitedString.shift());
+		}
+		log(`Splited array is parsed: ${JSON.stringify(dirtArgs)}`);
+		const args = this._clearArgs(dirtArgs, argsDef);
+		log(`Parsing complete ${JSON.stringify(args)}`);
+		return args;
 	}
-	charArr.shift();
-	return {
-		name: name.join(''),
-		value: value.join(''),
-	};
 }
 
-function parseUnNamed(charArr: Array<string>, to = 0) {
-	const value: Array<string> = [];
-	if (charArr[0] === pOptions.commonSeparator) charArr.splice(0, pOptions.commonSeparator.length);
-
-	while (charArr[0] !== pOptions.commonSeparator && charArr.length > 0) {
-		value.push(charArr.shift());
-	}
-	charArr.shift();
-	return value.join('');
-}
-
-function nParse(charArr: Array<string>, args: argsObj): argsObj {
-	if (charArr.length === 0) return args;
-	const char = charArr[0];
-	if (char === pOptions.namedSeparator) {
-		args.named.push(parseNamed(charArr));
-		return nParse(charArr, args);
-	} else {
-		args.unNamed.push(parseUnNamed(charArr));
-		return nParse(charArr, args);
-	}
-}
-
-export function parser(string: string, argsDef?: Array<argsDef>, options?: options) {
-	log('parsing start');
-	pOptions = { ...pOptions, ...options };
-	const charArr = string.split('');
-	customSplit(string);
-	const args: argsObj = {
-		named: [],
-		unNamed: [],
-	};
-	nParse(charArr, args);
-
-	log('parsing complete');
-	log(args);
-	// if (pOptions.includeCmd) {
-	// 	const getCmdResult = getCmd(parseCharArr, pOptions.prefix);
-	// 	result.cmd = getCmdResult.cmd;
-	// 	parseCharArr = getCmdResult.charArr;
-	// }
-}
-export default parser;
-
-parser('"dddd dsadsa" get -t "dsadawes dsaewsdwa ddd" named "remote add https://dddd.dd" dddadsd');
+export default Parser;
